8afc2c71e72181c8cc7174886acdc851fff384ea
Abdulholiq13 <abdulholiq571@gmail.com>
2023-08-11 10:58:24 +0500

    Add img-showcase and lightbox gallery

--- js/main.js
+++ js/main.js
@@ -21,13 +21,17 @@ if (elSiteHeaderCartLink) {
 }
 
 // Img showcase
-const elImgShowcaseActiveImg = document.querySelector(
+const elProductPageGallery = document.querySelector(".product-page__gallery");
+const elImgShowcaseActiveImg = elProductPageGallery.querySelector(
   ".img-showcase__active-img"
 );
-const elsImgShowcaseThumbnailButton = document.querySelectorAll(
+const elsImgShowcaseThumbnailButton = elProductPageGallery.querySelectorAll(
   ".js-img-showcase-thumbnail-button"
 );
-const elsImgThumbnail = document.querySelectorAll(".img-showcase__thumbnail");
+
+const elsImgThumbnail = elProductPageGallery.querySelectorAll(
+  ".img-showcase__thumbnail"
+);
 
 elsImgShowcaseThumbnailButton.forEach(function (elButton) {
   elButton.addEventListener("click", function () {
@@ -56,5 +60,83 @@ if (elLightboxToggler) {
 if (elLightBoxClose) {
   elLightBoxClose.addEventListener("click", function () {
     elLightBox.classList.remove(modifier.lightBoxOpen);
+  });
+}
+
+const elImgLightboxActiveImg = elLightBox.querySelector(
+  ".img-showcase__active-img--lightbox"
+);
+const elsImgLightboxThumbnailButton = elLightBox.querySelectorAll(
+  ".js-img-lightbox-thumbnail-button"
+);
+const elsLightboxImgThumbnail = elLightBox.querySelectorAll(
+  ".img-showcase__thumbnail"
+);
+
+elsImgLightboxThumbnailButton.forEach(function (elButton) {
+  elButton.addEventListener("click", function () {
+    elsLightboxImgThumbnail.forEach(function (elImgThumbnail) {
+      elImgThumbnail.classList.remove(modifier.imgThumbnailActive);
+    });
+
+    elButton.parentElement.classList.add(modifier.imgThumbnailActive);
+
+    elImgLightboxActiveImg.src = elButton.dataset.imgShowcaseBig;
+    elImgLightboxActiveImg.srcset = `${elButton.dataset.imgShowcaseBig} 1x, ${elButton.dataset.imgShowcaseRetina} 2x`;
+  });
+});
+
+// Lightbox control
+const elLightboxControlPrev = elLightBox.querySelector(
+  ".js-lightbox-control-prev"
+);
+const elLightboxControlNext = elLightBox.querySelector(
+  ".js-lightbox-control-next"
+);
+
+let elNextActiveItem;
+
+if (elLightboxControlNext) {
+  elLightboxControlNext.addEventListener("click", function () {
+    //Find active li element
+    const elActiveItem = elLightBox.querySelector(
+      ".img-showcase__thumbnail--active"
+    );
+    //Remove active li element's styles
+    elActiveItem.classList.remove(modifier.imgThumbnailActive);
+
+    if (elActiveItem.nextElementSibling === null) {
+      elNextActiveItem = elsLightboxImgThumbnail[0];
+    } else {
+      elNextActiveItem = elActiveItem.nextElementSibling;
+    }
+    elNextActiveItem.classList.add(modifier.imgThumbnailActive);
+
+    elImgLightboxActiveImg.src =
+      elNextActiveItem.children[0].dataset.imgShowcaseBig;
+    elImgLightboxActiveImg.srcset = `${elNextActiveItem.children[0].dataset.imgShowcaseBig} 1x, ${elNextActiveItem.children[0].dataset.imgShowcaseRetina} 2x`;
+  });
+}
+
+if (elLightboxControlPrev) {
+  elLightboxControlPrev.addEventListener("click", function () {
+    //Find active li element
+    const elActiveItem = elLightBox.querySelector(
+      ".img-showcase__thumbnail--active"
+    );
+    //Remove active li element's styles
+    elActiveItem.classList.remove(modifier.imgThumbnailActive);
+
+    if (elActiveItem.previousElementSibling === null) {
+      elNextActiveItem =
+        elsLightboxImgThumbnail[elsLightboxImgThumbnail.length - 1];
+    } else {
+      elNextActiveItem = elActiveItem.previousElementSibling;
+    }
+    elNextActiveItem.classList.add(modifier.imgThumbnailActive);
+
+    elImgLightboxActiveImg.src =
+      elNextActiveItem.children[0].dataset.imgShowcaseBig;
+    elImgLightboxActiveImg.srcset = `${elNextActiveItem.children[0].dataset.imgShowcaseBig} 1x, ${elNextActiveItem.children[0].dataset.imgShowcaseRetina} 2x`;
   });
 }
